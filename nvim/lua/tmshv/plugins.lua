a8f8a0f20baad77168c686e448d558822dedefee
Roman Timashev <roman@tmshv.ru>
2023-01-13 15:54:56 +0600

    update comments

--- nvim/lua/tmshv/plugins.lua
+++ nvim/lua/tmshv/plugins.lua
@@ -18,11 +18,11 @@ return function(use)
 
     -- AUTOCOMPLETE (nvim-cmp based)
     -- TODO: make it opt as posible
-    use "hrsh7th/nvim-cmp"             -- The completion plugin
-    use "hrsh7th/cmp-buffer"           -- buffer completions
-    use "hrsh7th/cmp-path"             -- path completions
-    use "hrsh7th/cmp-cmdline"          -- cmdline completions
-    use "saadparwaiz1/cmp_luasnip"     -- snippet completions
+    use "hrsh7th/nvim-cmp"             -- completion plugin
+    use "hrsh7th/cmp-buffer"           -- buffer completions source
+    use "hrsh7th/cmp-path"             -- path completions source
+    use "hrsh7th/cmp-cmdline"          -- cmdline completions source
+    use "saadparwaiz1/cmp_luasnip"     -- snippet completions source
 
     -- SNIPPETS
     use "L3MON4D3/LuaSnip"             --snippet engine
