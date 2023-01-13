e587b9f9432469cadb649c502de7e7387d88e789
Roman Timashev <roman@tmshv.ru>
2023-01-13 22:25:07 +0600

    add demos for lsp config

--- nvim/lua/tmshv/lsp.lua
+++ nvim/lua/tmshv/lsp.lua
@@ -5,17 +5,36 @@ if not status_ok then
 end
 
 require("mason").setup()
-require("mason-lspconfig").setup()
+require("mason-lspconfig").setup({
+    -- ensure_installed = {
+    -- "sumneko_lua",
+    -- "pyright",
+    -- "rust_analyzer",
+    -- "tsserver",
+    -- },
+})
 
 local lsp = require('lsp-zero')
 lsp.preset('recommended')
+
+-- how to configure a language server
+lsp.configure('tsserver', {
+  on_attach = function(client, bufnr)
+    -- print('hello tsserver')
+  end,
+  -- settings = {
+  --   completions = {
+  --     completeFunctionCalls = true
+  --   }
+  -- }
+})
 
 -- configure lua language server for neovim
 lsp.nvim_workspace()
 
 lsp.setup()
--- Learn the keybindings, see :help lsp-zero-keybindings
--- Learn to configure LSP servers, see :help lsp-zero-api-showcase
+-- learn the keybindings: :help lsp-zero-keybindings
+-- learn to configure LSP servers: :help lsp-zero-api-showcase
 
 -- Reserve space for diagnostic icons (LSP):
 vim.opt.signcolumn = "yes"                      -- always show the sign column, otherwise it would shift the text each time
