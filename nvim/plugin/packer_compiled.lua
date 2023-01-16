2e9c53c31221ddf5792ac59c2b523b546b90ca37
Roman Timashev <roman@tmshv.ru>
2023-01-16 18:38:27 +0600

    comment fix

--- nvim/plugin/packer_compiled.lua
+++ nvim/plugin/packer_compiled.lua
@@ -169,6 +169,11 @@ _G.packer_plugins = {
     path = "/Users/tmshv/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
     url = "https://github.com/nvim-treesitter/nvim-treesitter"
   },
+  ["nvim-ts-context-commentstring"] = {
+    loaded = true,
+    path = "/Users/tmshv/.local/share/nvim/site/pack/packer/start/nvim-ts-context-commentstring",
+    url = "https://github.com/JoosepAlviste/nvim-ts-context-commentstring"
+  },
   ["nvim-ts-rainbow"] = {
     loaded = true,
     path = "/Users/tmshv/.local/share/nvim/site/pack/packer/start/nvim-ts-rainbow",
