e24f7e97c62569175253363e59395db29b1e6f2d
Roman Timashev <roman@tmshv.ru>
2023-01-17 02:08:19 +0600

    comment fix but context is disabled

--- nvim/lua/tmshv/comment.lua
+++ nvim/lua/tmshv/comment.lua
@@ -1,30 +1,47 @@
 local status_ok, comment = pcall(require, "Comment")
 if not status_ok then
-  return
+    return
 end
 
 comment.setup {
+    padding = true,
+    sticky = true,
+    ignore = nil,
     toggler = {
         ---Line-comment toggle keymap
         line = "<leader>/",
         ---Block-comment toggle keymap
-        --[[ block = "<leader>/", ]]
+        -- block = "<leader>c/",
     },
-  -- setup context for JSX commenting
-  pre_hook = function(ctx)
-    local U = require "Comment.utils"
+    opleader = {
+        line = "<leader>/",
+        -- block = "<leader>c",
+    },
+    extra = {
+        above = 'gcO',
+        below = 'gco',
+        eol = 'gcA',
+    },
+    mappings = {
+        basic = true,
+        extra = false,
+    },
+    -- setup context for JSX commenting
+    pre_hook = function(ctx)
+        local U = require "Comment.utils"
 
-    local location = nil
-    if ctx.ctype == U.ctype.block then
-      location = require("ts_context_commentstring.utils").get_cursor_location()
-    elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
-      location = require("ts_context_commentstring.utils").get_visual_start_location()
-    end
+        local location = nil
+        if ctx.ctype == U.ctype.block then
+            location = require("ts_context_commentstring.utils").get_cursor_location()
+        elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
+            location = require("ts_context_commentstring.utils").get_visual_start_location()
+        end
 
-    return require("ts_context_commentstring.internal").calculate_commentstring {
-      key = ctx.ctype == U.ctype.line and "__default" or "__multiline",
-      location = location,
-    }
-  end,
+        return
+        --[[ return require("ts_context_commentstring.internal").calculate_commentstring { ]]
+        --[[     key = ctx.ctype == U.ctype.line and "__default" or "__multiline", ]]
+        --[[     location = location, ]]
+        --[[ } ]]
+    end,
+    post_hook = nil,
 }
-
