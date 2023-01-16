9470b2181fc12513c887911f718d248d39bbf0ee
Roman Timashev <roman@tmshv.ru>
2023-01-16 23:40:04 +0600

    add gitsigns

--- nvim/plugin/packer_compiled.lua
+++ nvim/plugin/packer_compiled.lua
@@ -129,6 +129,11 @@ _G.packer_plugins = {
     path = "/Users/tmshv/.local/share/nvim/site/pack/packer/start/friendly-snippets",
     url = "https://github.com/rafamadriz/friendly-snippets"
   },
+  ["gitsigns.nvim"] = {
+    loaded = true,
+    path = "/Users/tmshv/.local/share/nvim/site/pack/packer/start/gitsigns.nvim",
+    url = "https://github.com/lewis6991/gitsigns.nvim"
+  },
   ["indent-blankline.nvim"] = {
     loaded = true,
     path = "/Users/tmshv/.local/share/nvim/site/pack/packer/start/indent-blankline.nvim",
