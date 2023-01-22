86efc22fd2cc0414301957547f10135850db461a
Roman Timashev <roman@tmshv.ru>
2023-01-22 14:51:50 +0600

    add a keymap to find with hidden files

--- nvim/lua/tmshv/telescope.lua
+++ nvim/lua/tmshv/telescope.lua
@@ -79,15 +79,21 @@ telescope.setup {
             },
         },
     },
-    -- pickers = {
     -- Default configuration for builtin pickers goes here:
-    -- picker_name = {
-    --   picker_config_key = value,
-    --   ...
-    -- }
-    -- Now the picker_config_key will be applied every time you call this
-    -- builtin picker
-    -- },
+    pickers = {
+        -- picker_name = {
+        --   picker_config_key = value,
+        --   ...
+        -- }
+        -- Now the picker_config_key will be applied every time you call this builtin picker
+        find_files = {
+            hidden = false,
+            file_ignore_patterns = {
+                "node_modules",
+                ".git",
+            },
+        },
+    },
     extensions = {
         -- Your extension configuration goes here:
         -- extension_name = {
@@ -99,6 +105,7 @@ telescope.setup {
 
 local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+vim.keymap.set("n", "<leader>fF", "<cmd>Telescope find_files hidden=true <CR>", {})
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
