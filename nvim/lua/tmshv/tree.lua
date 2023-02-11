5c10e24b7655cd8407b9bb32c574bef15a15c4d1
Roman Timashev <roman@tmshv.ru>
2023-02-11 17:29:08 +0500

    rm deprecated options from tree

--- nvim/lua/tmshv/tree.lua
+++ nvim/lua/tmshv/tree.lua
@@ -17,12 +17,6 @@ tree.setup({
     },
     disable_netrw = true,
     hijack_netrw = true,
-    open_on_setup = false,
-    ignore_ft_on_setup = {
-        "startify",
-        "dashboard",
-        "alpha",
-    },
     open_on_tab = false,
     hijack_cursor = false,
     update_cwd = true,
