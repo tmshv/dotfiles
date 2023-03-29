c1f2286a3153a2c2c67f4213d11e8a20571ef5ed
Roman Timashev <roman@tmshv.ru>
2023-03-29 13:21:34 +0530

    fix wrong options

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -43,26 +43,26 @@ return {
 	},
 
 	tab_bar_style = {
-		active_tab_left = wezterm.format {
-			{ Background = { Color = '#fa00fa' } },
-			{ Foreground = { Color = '#2b2042' } },
-			{ Text = SOLID_LEFT_ARROW },
-		},
-		active_tab_right = wezterm.format {
-			{ Background = { Color = '#0b0022' } },
-			{ Foreground = { Color = '#2b2042' } },
-			{ Text = SOLID_RIGHT_ARROW },
-		},
-		inactive_tab_left = wezterm.format {
-			{ Background = { Color = '#0b0022' } },
-			{ Foreground = { Color = '#1b1032' } },
-			{ Text = SOLID_LEFT_ARROW },
-		},
-		inactive_tab_right = wezterm.format {
-			{ Background = { Color = '#0b0022' } },
-			{ Foreground = { Color = '#1b1032' } },
-			{ Text = SOLID_RIGHT_ARROW },
-		},
+		-- active_tab_left = wezterm.format {
+		-- 	{ Background = { Color = '#fa00fa' } },
+		-- 	{ Foreground = { Color = '#2b2042' } },
+		-- 	{ Text = SOLID_LEFT_ARROW },
+		-- },
+		-- active_tab_right = wezterm.format {
+		-- 	{ Background = { Color = '#0b0022' } },
+		-- 	{ Foreground = { Color = '#2b2042' } },
+		-- 	{ Text = SOLID_RIGHT_ARROW },
+		-- },
+		-- inactive_tab_left = wezterm.format {
+		-- 	{ Background = { Color = '#0b0022' } },
+		-- 	{ Foreground = { Color = '#1b1032' } },
+		-- 	{ Text = SOLID_LEFT_ARROW },
+		-- },
+		-- inactive_tab_right = wezterm.format {
+		-- 	{ Background = { Color = '#0b0022' } },
+		-- 	{ Foreground = { Color = '#1b1032' } },
+		-- 	{ Text = SOLID_RIGHT_ARROW },
+		-- },
 	},
 
 	-- window_background_opacity = 0.95,
