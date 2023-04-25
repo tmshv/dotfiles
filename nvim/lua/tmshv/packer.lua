2597ca12f7e4d8a0a1e7318598f301baf7d4501c
Roman Timashev <roman@tmshv.ru>
2023-04-26 01:28:07 +0530

    format

--- nvim/lua/tmshv/packer.lua
+++ nvim/lua/tmshv/packer.lua
@@ -6,22 +6,22 @@ local fn = vim.fn
 -- this part of code is taken from https://github.com/wbthomason/packer.nvim
 -- BUT CHANGED A BIT TO LOOKS BETTER
 local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system {
-      "git",
-      "clone",
-      "--depth",
-      "1",
-      "https://github.com/wbthomason/packer.nvim",
-      install_path,
-    }
-    print("Installing packer close and reopen Neovim...")
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
+    local fn = vim.fn
+    local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+    if fn.empty(fn.glob(install_path)) > 0 then
+        fn.system {
+            "git",
+            "clone",
+            "--depth",
+            "1",
+            "https://github.com/wbthomason/packer.nvim",
+            install_path,
+        }
+        print("Installing packer close and reopen Neovim...")
+        vim.cmd [[packadd packer.nvim]]
+        return true
+    end
+    return false
 end
 local PACKER_BOOTSTRAP = ensure_packer()
 
@@ -39,27 +39,27 @@ vim.cmd [[
 -- !!!
 local status_ok, packer = pcall(require, "packer")
 if not status_ok then
-  return
+    return
 end
 
 -- have packer use a popup window
 packer.init {
-  display = {
-    open_fn = function()
-      return require("packer.util").float { border = "rounded" }
-    end,
-  },
+    display = {
+        open_fn = function()
+            return require("packer.util").float { border = "rounded" }
+        end,
+    },
 }
 
 -- Install your plugins here
 return function(plugins)
-  return packer.startup(function(use)
-    plugins(use)
+    return packer.startup(function(use)
+        plugins(use)
 
-    -- automatically set up your configuration after cloning packer.nvim
-    -- keep this at the end after all plugins
-    if PACKER_BOOTSTRAP then
-      packer.sync()
-    end
-  end)
+        -- automatically set up your configuration after cloning packer.nvim
+        -- keep this at the end after all plugins
+        if PACKER_BOOTSTRAP then
+            packer.sync()
+        end
+    end)
 end
