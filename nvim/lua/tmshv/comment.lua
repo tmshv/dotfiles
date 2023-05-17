b990fb2e4dd5b94cbb4b5f4fce5101d797e10957
Roman Timashev <roman@tmshv.ru>
2023-05-17 03:51:28 +0300

    comment update

--- nvim/lua/tmshv/comment.lua
+++ nvim/lua/tmshv/comment.lua
@@ -5,34 +5,46 @@ end
 
 -- setup context for JSX commenting
 local pre_hook = nil
-local status_ok, ts_context_commentstring = pcall(require, "ts_context_commentstring.integrations.comment_nvim")
-if status_ok then
+local status_ok_tscc, ts_context_commentstring = pcall(require, "ts_context_commentstring.integrations.comment_nvim")
+if status_ok_tscc then
     pre_hook = ts_context_commentstring.create_pre_hook()
 end
 
 comment.setup {
+    ---Add a space b/w comment and the line
     padding = true,
+    ---Whether the cursor should stay at its position
     sticky = true,
+    ---Lines to be ignored while (un)comment
     ignore = nil,
+    ---LHS of toggle mappings in NORMAL mode
     toggler = {
         ---Line-comment toggle keymap
         line = "<leader>/",
         ---Block-comment toggle keymap
         -- block = "<leader>c/",
     },
+    ---LHS of operator-pending mappings in NORMAL and VISUAL mode
     opleader = {
         line = "<leader>/",
         -- block = "<leader>c",
     },
-    extra = {
-        above = 'gcO',
-        below = 'gco',
-        eol = 'gcA',
-    },
+    ---LHS of extra mappings
+    -- extra = {
+    --     above = 'gcO',
+    --     below = 'gco',
+    --     eol = 'gcA',
+    -- },
+    ---Enable keybindings
+    ---NOTE: If given `false` then the plugin won't create any mappings
     mappings = {
+        ---Operator-pending mapping; `gcc` `gbc` `gc[count]{motion}` `gb[count]{motion}`
         basic = true,
+        ---Extra mapping; `gco`, `gcO`, `gcA`
         extra = false,
     },
+     ---Function to call before (un)comment
     pre_hook = pre_hook,
+    ---Function to call after (un)comment
     post_hook = nil,
 }
