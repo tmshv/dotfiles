f81a881d5e3686f2b5a2166b99f9add73fbb8df3
Roman Timashev <roman@tmshv.ru>
2023-09-04 17:57:06 +0300

    clean tree config

--- nvim/lua/tmshv/tree.lua
+++ nvim/lua/tmshv/tree.lua
@@ -3,18 +3,6 @@ if not status_ok then
     return
 end
 
--- local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
--- if not config_status_ok then
---     return
--- end
-
--- local tree_cb = nvim_tree_config.nvim_tree_callback
-
--- This function has been generated from your
---   view.mappings.list
---   view.mappings.custom_only
---   remove_keymaps
--- Please see https://github.com/nvim-tree/nvim-tree.lua/wiki/Migrating-To-on_attach for assistance in migrating.
 local function on_attach(bufnr)
     local api = require('nvim-tree.api')
 
@@ -22,15 +10,6 @@ local function on_attach(bufnr)
         return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
     end
 
-    -- list = {
-    --     { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
-    --     { key = "h",                  cb = tree_cb "close_node" },
-    --     --[[ { key = "v", cb = tree_cb "vsplit" }, ]]
-    -- },
-
-    -- Default mappings. Feel free to modify or remove as you wish.
-    --
-    -- BEGIN_DEFAULT_ON_ATTACH
     vim.keymap.set('n', '<C-]>', api.tree.change_root_to_node, opts('CD'))
     vim.keymap.set('n', '<C-e>', api.node.open.replace_tree_buffer, opts('Open: In Place'))
     vim.keymap.set('n', '<C-k>', api.node.show_info_popup, opts('Info'))
@@ -83,34 +62,6 @@ local function on_attach(bufnr)
     vim.keymap.set('n', 'Y', api.fs.copy.relative_path, opts('Copy Relative Path'))
     vim.keymap.set('n', '<2-LeftMouse>', api.node.open.edit, opts('Open'))
     vim.keymap.set('n', '<2-RightMouse>', api.tree.change_root_to_node, opts('CD'))
-    -- END_DEFAULT_ON_ATTACH
-
-
-    -- Mappings removed via:
-    --   remove_keymaps
-    --   OR
-    --   view.mappings.list..action = ""
-    --
-    -- The dummy set before del is done for safety, in case a default mapping does not exist.
-    --
-    -- You might tidy things by removing these along with their default mapping.
-    vim.keymap.set('n', 'O', '', { buffer = bufnr })
-    vim.keymap.del('n', 'O', { buffer = bufnr })
-    vim.keymap.set('n', '<2-RightMouse>', '', { buffer = bufnr })
-    vim.keymap.del('n', '<2-RightMouse>', { buffer = bufnr })
-    vim.keymap.set('n', 'D', '', { buffer = bufnr })
-    vim.keymap.del('n', 'D', { buffer = bufnr })
-    vim.keymap.set('n', 'E', '', { buffer = bufnr })
-    vim.keymap.del('n', 'E', { buffer = bufnr })
-
-
-    -- Mappings migrated from view.mappings.list
-    vim.keymap.set('n', 'l', api.node.open.edit, opts('Open'))
-    vim.keymap.set('n', '<CR>', api.node.open.edit, opts('Open'))
-    vim.keymap.set('n', 'o', api.node.open.edit, opts('Open'))
-    vim.keymap.set('n', 'h', api.node.navigate.parent_close, opts('Close Directory'))
-
-    vim.keymap.set('n', 'Z', api.node.run.system, opts('Run System'))
 end
 
 tree.setup({
@@ -150,14 +101,6 @@ tree.setup({
     view = {
         width = 40,
         side = "left",
-        mappings = {
-            custom_only = false,
-            -- list = {
-            --     { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
-            --     { key = "h",                  cb = tree_cb "close_node" },
-            --[[ { key = "v", cb = tree_cb "vsplit" }, ]]
-            -- },
-        },
         number = false,
         relativenumber = false,
     },
