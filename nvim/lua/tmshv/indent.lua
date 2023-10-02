f6d4b238881c92d716732a422aa8f681503a4061
Roman Timashev <roman@tmshv.ru>
2023-10-02 22:21:05 +0300

    indent config upd

--- nvim/lua/tmshv/indent.lua
+++ nvim/lua/tmshv/indent.lua
@@ -1,16 +1,7 @@
-local status_ok, indent = pcall(require, "indent_blankline")
+local status_ok, indent = pcall(require, "ibl")
 if not status_ok then
-  return
+    return
 end
 
--- show EOL character
-vim.opt.list = true
--- vim.opt.listchars:append "eol:↴"
-
-indent.setup {
-    -- for example, context is off by default, use this to turn it on
-    show_current_context = true,
-    show_current_context_start = true,
-    show_end_of_line = true, -- show EOL character
-}
+indent.setup()
 
