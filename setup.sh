7c47b6e3ad225dd695138cbf4a4d59c5f2074fc0
Roman Timashev <roman@tmshv.ru>
2026-02-10 16:35:51 +0700

    upd omz repo

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 git config --global user.name "Roman Timashev"
 git config --global user.email "roman@tmshv.ru"
